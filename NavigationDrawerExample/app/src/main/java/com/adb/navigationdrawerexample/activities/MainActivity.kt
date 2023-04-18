44d167448d375cdd55df7e9f5b8f8428092aca21
matiasilveiro <matias.silveiro@gmail.com>
2023-04-18 19:48:59 -0300

    Updated Navigation Drawer example

--- NavigationDrawerExample/app/src/main/java/com/adb/navigationdrawerexample/activities/MainActivity.kt
+++ NavigationDrawerExample/app/src/main/java/com/adb/navigationdrawerexample/activities/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.adb.navigationdrawerexample.activities
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
@@ -9,43 +8,78 @@ import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.adb.navigationdrawerexample.R
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.*
-import android.view.Menu;
-
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+
+    private val topLevelDestinations = setOf(
+        R.id.fragment1,
+        R.id.fragment2
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navView = findViewById(R.id.nav_view)
+        drawerLayout = findViewById(R.id.drawer_layout)
 
-        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
-
-        nav_view.setupWithNavController(navController)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+        setupSimpleNavigation()
+        //setupComplexNavigation()
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, drawer_layout)
+        return if (::appBarConfiguration.isInitialized) {
+            NavigationUI.navigateUp(navController, appBarConfiguration)
+        } else {
+            NavigationUI.navigateUp(navController, drawerLayout)
+        }
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.drawe_menu, menu)
-        return true
-    }
-
 
     override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
+        }
+    }
+
+    private fun setupSimpleNavigation() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
+
+    private fun setupComplexNavigation() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        appBarConfiguration = AppBarConfiguration.Builder(topLevelDestinations)
+            .setDrawerLayout(drawerLayout)
+            .build()
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawerLayout.closeDrawers()
+
+            when (val id = menuItem.itemId) {
+                R.id.test -> Snackbar.make(navView, "Test", Snackbar.LENGTH_SHORT).show()
+                else -> navController.navigate(id)
+            }
+            return@setNavigationItemSelectedListener true
         }
     }
 }
